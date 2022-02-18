a27ff16139d99cc7b47a0ed8c50c9b356285f9d0
Santiago <santiagofgaray@gmail.com>
2022-02-18 16:46:06 +0100

    update db config

--- config/db.js
+++ config/db.js
@@ -5,16 +5,11 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URL = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
+const MONGO_URL = `${process.env.MONGODB_URI}`; ///${process.env.DB_NAME}
 
 console.log("MONGO_URL: ", MONGO_URL)
 mongoose
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useFindAndModify: false,
-    // useCreateIndex: true
-  })
+  .connect(MONGO_URL)
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
