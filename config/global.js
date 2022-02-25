0e9d277e19aea24eaa6287c270db431f05b407c0
Santiago <santiagofgaray@gmail.com>
2022-02-25 12:26:32 +0100

    capitalize helper updated

--- config/global.js
+++ config/global.js
@@ -1,8 +1,8 @@
-var express = require('express');
-const app= express();
-var cors = require('cors');
+var express = require("express");
+const app = express();
+var cors = require("cors");
 
-var logger = require('morgan');
+var logger = require("morgan");
 
 const cookieParser = require("cookie-parser");
 
@@ -10,16 +10,16 @@ const favicon = require("serve-favicon");
 
 const path = require("path");
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-const hbs = require('hbs')
+const hbs = require("hbs");
 
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
 
-  app.use(cors())
+  app.use(cors());
   app.use(logger("dev"));
 
   // To have access to `body` property in the request
@@ -32,26 +32,36 @@ module.exports = (app) => {
   // Sets the view engine to handlebars
   app.set("view engine", "hbs");
 
-  hbs.registerPartials(path.join(__dirname, '../views/partials'))
-  hbs.registerHelper('capitalize',function(name){
-      return name[0].toUpperCase()+name.slice(1);
-  })
-  hbs.registerHelper('math', function(bac) {
-    if (bac > 0.25) { 
-      return bac / 0.25}
-    else return 0;
-  })
+  hbs.registerPartials(path.join(__dirname, "../views/partials"));
+
+  hbs.registerHelper("capitalize", function (name) {
+    let cuts = name.split(" ");
+    for (let i = 0; i < cuts.length; i++) {
+      let j = cuts[i].charAt(0).toUpperCase();
+      cuts[i] = j + cuts[i].substr(1);
+    }
+    return cuts.join(" ");
+  });
+
+
+  hbs.registerHelper("math", function (bac) {
+    if (bac > 0.25) {
+      return bac / 0.25;
+    } else return 0;
+  });
 
   // Handles access to the public folder
   app.use(express.static(path.join(__dirname, "..", "public")));
 
   // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  app.use(
+    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
+  );
 
   app.use(
     session({
       cookie: {
-        maxAge: 24 * 60 * 60 * 1000
+        maxAge: 24 * 60 * 60 * 1000,
       },
       secret: process.env.SESSION_SECRET || "super hyper secret key",
       resave: false,
@@ -62,4 +72,3 @@ module.exports = (app) => {
     })
   );
 };
-
